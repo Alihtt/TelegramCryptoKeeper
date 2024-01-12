a6f426c558c044fcf53ce94ea7f907937f4d9e97
Alihtt <Aliht.workspace@gmail.com>
2024-01-12 16:52:20 +0330

    update bot messages

--- plugins/messages/messages.py
+++ plugins/messages/messages.py
@@ -10,7 +10,7 @@ SUPPORTED_BOTS = {'wallet': 1985737506, 'xrocket': 5014831088, 'send': 155950163
 
 @Client.on_message(filters.channel)
 async def channel_messages(bot: Client, message: Message):
-    pattern = r'https://t.me/([^/?]+)\?start=(\w+)'
+    pattern = r'https://t.me/([^/?]+)\?start=(.+)'
 
     if message.reply_markup:
         text = message.reply_markup.inline_keyboard[0][0].url
@@ -19,20 +19,21 @@ async def channel_messages(bot: Client, message: Message):
 
     match = re.search(pattern, text)
     if match:
+        logging.info(f'This Token is activating:\n{text}')
         extracted_username = match.group(1)
         extracted_code = match.group(2)
         if extracted_username.lower() in SUPPORTED_BOTS:
-            logging.info("Extracted Username: " + extracted_username)
-            logging.info("Extracted Code: " + extracted_code)
+            logging.info('Extracted Username: ' + extracted_username)
+            logging.info('Extracted Code: ' + extracted_code)
 
             await bot.send_message(chat_id=extracted_username, text=f'/start {extracted_code}')
             await bot.send_message('self', f'This Token is activating \n{text}')
 
 
-@Client.on_message()
+@Client.on_message(filters.inline_keyboard)
 async def bot_messages(bot: Client, message: Message):
     if message.chat.id in SUPPORTED_BOTS.values():
-        if message.reply_markup:
+        if 'activate' in message.text:
             try:
                 for keyboard in message.reply_markup.inline_keyboard[0]:
                     if keyboard.url:
